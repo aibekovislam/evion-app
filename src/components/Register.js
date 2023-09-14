ec55ff6bd5c9f8d046bbf92ee8060c914a5be551
Islam Aibekov <islamaibekov@Islams-MacBook-Air.local>
2023-09-14 11:03:05 +0600

    added sms verify

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { View, Text, TextInput, Button, Alert, ScrollView } from 'react-native';
 import axios from 'axios';
+import { styles } from '../styles/AuthorizationStyles';
+import { useNavigation } from '@react-navigation/native';
 
 const RegisterScreen = () => {
   const [username, setUsername] = useState('');
   const [phone, setPhone] = useState('');
   const [password, setPassword] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+  const [ changeWindow, setChangeWindow ] = useState(false);
+  const navigation = useNavigation();
 
   const handleRegister = async () => {
     try {
@@ -14,35 +19,67 @@ const RegisterScreen = () => {
         phone,
         password,
       });
-
-      Alert.alert('Успешная регистрация', 'Пользователь зарегистрирован успешно');
+      setChangeWindow(true)
     } catch (error) {
       console.error(error);
       Alert.alert('Ошибка', 'Что-то пошло не так. Попробуйте еще раз.');
     }
   };
 
+  const handleVerify = async () => {
+    try {
+      const response = await axios.post('https://eviona-pp-25e208506c12.herokuapp.com/verify', {
+        phone, 
+        verificationCode,
+      });
+      navigation.navigate('Home');
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Ошибка', 'Неверный код верификации. Попробуйте еще раз.');
+    }
+  };
+
   return (
-    <View>
-      <Text>Register Screen</Text>
-      <TextInput
-        placeholder="Username"
-        onChangeText={text => setUsername(text)}
-        value={username}
-      />
-      <TextInput
-        placeholder="Phone Number"
-        onChangeText={text => setPhone(text)}
-        value={phone}
-      />
-      <TextInput
-        placeholder="Password"
-        onChangeText={text => setPassword(text)}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Register" onPress={handleRegister} />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>{ !changeWindow ? "Регистрация" : "Верификация" }</Text>
+      
+      { !changeWindow && (
+        <>
+          <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={text => setUsername(text)}
+          value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Phone Number"
+            onChangeText={text => setPhone(text)}
+            value={phone}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            onChangeText={text => setPassword(text)}
+            value={password}
+            secureTextEntry
+          />
+          <Button title="Register" onPress={handleRegister} />
+        </>
+      ) }
+
+      {changeWindow && (
+        <>
+          <TextInput
+            style={styles.input}
+            placeholder="Verification Code"
+            onChangeText={text => setVerificationCode(text)}
+            value={verificationCode}
+          />
+          <Button title="Verify" onPress={handleVerify} />
+        </>
+      )}
+    </ScrollView>
   );
 };
 
