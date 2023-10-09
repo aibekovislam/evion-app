c08ac39a2106fa485d4545dfeec502005244b76a
aibekovislam <islamxdaamduu@gmail.com>
2023-10-09 20:18:11 +0600

    added direction geolocation

--- src/components/MapScreen.js
+++ src/components/MapScreen.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, Animated } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { mapStyles } from '../styles/MapStyles';
-import { Accuracy, getCurrentPositionAsync, requestForegroundPermissionsAsync } from 'expo-location';
+import * as Location from 'expo-location'; 
 import MapViewDirections from 'react-native-maps-directions';
 import { styles } from '../styles/AuthorizationStyles';
 import MenuSVG from '../static/MenuSVG';
@@ -10,56 +10,96 @@ import GeoSVG from '../static/GeoSVG';
 import { GOOGLE_API_KEY } from '../utils/consts';
 import TaxCarSVG from '../static/tax_car';
 
-
 const MapScreen = ({ locations, selectedLocation }) => {
-  const [ userLocation, setUserLocation ] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const [heading, setHeading] = useState(0);
   const mapViewRef = useRef(null);
+  const rotateInterpolate = useRef(new Animated.Value(0)).current;
 
-  useEffect( () => {
-    (async() => {
-      let {status}= await requestForegroundPermissionsAsync();
-      if(status !=='granted')
-      {
-        return {error: 'Permission to access locatiob was denied'};
+  useEffect(() => {
+    const subscribeToLocationUpdates = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
       }
-      let location= await getCurrentPositionAsync({
-        accuracy: Accuracy.BestForNavigation,
-        maximumAge: 100
+
+      await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 5000,
+          distanceInterval: 10,
+        },
+        (newLocation) => {
+          const { coords } = newLocation;
+          setUserLocation(coords);
+        }
+      );
+    };
+
+    subscribeToLocationUpdates();
+  }, []);
+
+  useEffect(() => {
+    const headingSubscription = Location.watchHeadingAsync((data) => {
+      setHeading(data.magHeading);
+    });
+  
+    return () => {
+      if (headingSubscription) {
+        headingSubscription.remove && headingSubscription.remove();
+      }   
+    }
+  }, []);
+
+  useEffect(() => {
+    Animated.timing(rotateInterpolate, {
+      toValue: heading,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [heading]);
+
+  useEffect(() => {
+    if (userLocation) {
+      mapViewRef.current?.animateToRegion({
+        latitude: userLocation.latitude,
+        longitude: userLocation.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
       });
-      setUserLocation(location);
-    })();
-  },[]);
+    }
+  }, [userLocation]);
+
 
   useEffect(() => {
     if (userLocation) {
       centerMapOnUserLocation();
     }
   }, [userLocation]);  
-
-  const centerMapOnUserLocation = () => {
-    if (userLocation) {
-      mapViewRef.current?.animateToRegion({
-        latitude: userLocation.coords.latitude,
-        longitude: userLocation.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      });
-    }
-  };  
   
-  console.log(userLocation)
+  // console.log(userLocation)
+
+  const rotationStyle = {
+    transform: [
+      {
+        rotate: rotateInterpolate.interpolate({
+          inputRange: [0, 360],
+          outputRange: ['0deg', '360deg'],
+        }),
+      },
+    ],
+  };
 
   return (
     <View style={mapStyles.container}>
       <View style={styles.menu}>
-        <MenuSVG/>
-      </View>
-      <View style={styles.menu}>
-        <MenuSVG/>
+        <MenuSVG />
       </View>
       <View style={styles.geo_btn}>
-        <TouchableOpacity onPress={centerMapOnUserLocation}>
-          <GeoSVG/>
+        <TouchableOpacity onPress={() => {
+        }}>
+          <GeoSVG />
         </TouchableOpacity>
       </View>
       <MapView
@@ -67,20 +107,18 @@ const MapScreen = ({ locations, selectedLocation }) => {
         showsMyLocationButton={false}
         showsUserLocation={false}
         style={mapStyles.map}
-        mapPadding={{ top: 90, right: 20, bottom: 10, left: 20 }}
         initialRegion={{
-          latitude: 42.8751511,
-          longitude: 74.598045,
+          latitude: userLocation ? userLocation.latitude : 37.78825,
+          longitude: userLocation ? userLocation.longitude : -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        userLocationPriority={'high'}
-        userLocationFastestInterval={100}
-        showsCompass={false}
       >
         {userLocation ? (
-          <Marker anchor={{ x: 0.5, y: 0.5 }} coordinate={userLocation.coords}>
-            <TaxCarSVG />
+          <Marker coordinate={userLocation}>
+            <Animated.View style={rotationStyle}>
+              <TaxCarSVG />
+            </Animated.View>
           </Marker>
         ) : null}
         {locations?.map((location, index) => (
@@ -89,22 +127,23 @@ const MapScreen = ({ locations, selectedLocation }) => {
             coordinate={{ latitude: location?.latitude, longitude: location?.longitude }}
             title={location.name}
             description={"Станция Evion"}
-            // image={customMarkerImage}
           />
         ))}
-        <MapViewDirections
-          origin={userLocation?.coords}
-          destination={selectedLocation}
-          apikey={GOOGLE_API_KEY}
-          strokeWidth={5}
-          mode={'DRIVING'}
-        />
+        {userLocation && selectedLocation && (
+          <MapViewDirections
+            origin={userLocation}
+            destination={selectedLocation}
+            apikey={GOOGLE_API_KEY}
+            strokeWidth={5}
+            mode={'DRIVING'}
+          />
+        )}
       </MapView>
-        <View style={styles.countAllClients}>
-          <Text style={styles.small_text}>Кол-во</Text>
-          <Text style={styles.count_cli}>100</Text>
-          <Text style={styles.small_text}>Пассажиров</Text>
-        </View>
+      <View style={styles.countAllClients}>
+        <Text style={styles.small_text}>Кол-во</Text>
+        <Text style={styles.count_cli}>100</Text>
+        <Text style={styles.small_text}>Пассажиров</Text>
+      </View>
     </View>
   );
 };
